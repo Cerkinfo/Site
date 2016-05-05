88b0df86d8bf9e930fdb8125ea46a18b97c8d2e5
loispostula <lois.postula@railnova.eu>
2016-05-05 16:48:48 +0200

    Add base url for the cipedia

---
+++ members/urls.py
@@ -0,0 +1,29 @@
+from django.conf.urls import *
+from django.contrib.auth.decorators import login_required
+
+from cerkinfo_members.views import MemberDetailView, \
+    YearDetailView, CurrentMemberDetailView, MemberEditView, YearEditView
+
+urlpatterns = patterns(
+    '',
+    url(
+        r'^profile',
+        login_required(CurrentMemberDetailView.as_view()),
+        name='profile'),
+    url(
+        r'^member/(?P<slug>.+)',
+        MemberDetailView.as_view(),
+        name='member_profil'),
+    url(
+        r'^edit',
+        login_required(MemberEditView.as_view()),
+        name='user_edit'),
+    url(
+        r'^year',
+        YearDetailView.as_view(),
+        name='year_detail'),
+    url(
+        r'^manage_year',
+        login_required(YearEditView.as_view()),
+        name='year_edit'),
+)
