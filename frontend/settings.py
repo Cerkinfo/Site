0c9f018f29ee6b98a996dd2d2ad06139f8a2b318
loispostula <lois.postula@railnova.eu>
2016-05-08 17:32:33 +0200

    Change django admin to django suit

--- frontend/settings.py
+++ frontend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -94,6 +95,7 @@ TEMPLATES = [
                 'django.template.context_processors.tz',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.core.context_processors.request',
                 'django.contrib.messages.context_processors.messages',
                 'sekizai.context_processors.sekizai',
             ],
@@ -209,6 +211,13 @@ NOCAPTCHA = True
 RECAPTCHA_USE_SSL = True
 
 SITE_ID = 1
+
+# Django Suit Admin customization
+# Django Suit configuration example
+SUIT_CONFIG = {
+    # header
+    'ADMIN_NAME': 'Cerkinfo',
+}
 
 try:
     from local_settings import *  # pragma: no flakes
