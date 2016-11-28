41bc4ba3ac87ce85ef4e39eddb89108d3089bb2f
Perale Thomas <thomas.perale@openmailbox.org>
2016-11-28 19:51:21 +0100

    Basic permissions

---
+++ members/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework import permissions
+
+class IsOwner(permissions.BasePermission):
+    """
+    Custom permission to only allow owners to see it
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
+class IsBar(permissions.BasePermission):
+    """
+    Custom permission to allow barman only
+    """
+    def has_object_permission(self, request, view, obj):
+        return True
+
+class IsOwnerOrBar(permissions.BasePermission):
+    """
+    Custom permission to allow barman and owner only
+    """
+    def has_object_permission(self, request, view, obj):
+        return True
