78bd37fb09a242ed57a2f8ff3d032f48a0021ced
Perale Thomas <thomas.perale@openmailbox.org>
2016-12-02 15:27:46 +0100

    Fix dep

--- members/migrations/0010_auto_20161201_1524.py
+++ members/migrations/0010_auto_20161201_1524.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('members', '0010_member_is_bar'),
+        ('members', '0009_member_card_id'),
     ]
 
     operations = [
