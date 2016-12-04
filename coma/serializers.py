a1f01940c9cf4d9124d51d3b755eb2184f2c87e8
Perale Thomas <thomas.perale@openmailbox.org>
2016-12-04 17:20:28 +0100

    "fromWho" read only

--- coma/serializers.py
+++ coma/serializers.py
@@ -4,4 +4,5 @@ from coma.models import Transaction
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
+        read_only_fields = ('fromWho',)
         fields = ('user', 'fromWho', 'quantity', 'price', 'comment', 'date')
