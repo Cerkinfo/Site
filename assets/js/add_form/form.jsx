532a68ffb09df05c9fc15a50322872b0aefd830e
Perale Thomas <thomas.perale@openmailbox.org>
2017-03-03 19:16:02 +0100

    React bug fix

--- assets/js/add_form/form.jsx
+++ assets/js/add_form/form.jsx
@@ -29,7 +29,7 @@ export default class Container extends React.Component {
             this.setState({
                 messages: [
                     `Versement de ${Math.abs(r.price)}€ effectué avec succès.`
-                ]
+                ],
                 errors: [],
             });
         }).catch(error => {
