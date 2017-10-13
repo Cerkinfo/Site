3147e6e7d83f4492cb0d7b2d0b3fd64f6e949897
Thomas Perale <thomas.perale@openmailbox.org>
2017-10-13 20:06:44 +0200

    [fix] Avatar image

--- assets/js/profile/components/Avatar.js
+++ assets/js/profile/components/Avatar.js
@@ -3,25 +3,16 @@ import { connect } from 'react-redux';
 // import { membersOperations } from '../ducks/members/';
 import { CardTitle } from 'react-materialize';
 
-class Avatar extends React.Component {
-  render() {
-    return (
-      <div>
-        <CardTitle reveal image={this.props.self.avatar} waves='light'/>
-        <a href="" className="btn-floating btn halfway-fab waves-effect waves-light red">
-          <i className="material-icons">mode_edit</i>
-        </a>
-      </div>
-    );
-  }
+const Avatar = ({ avatar, }) => {
+  console.log(avatar);
+  return (
+    <div>
+      <CardTitle reveal image={avatar} waves='light'/>
+      <a href="" className="btn-floating btn halfway-fab waves-effect waves-light red">
+        <i className="material-icons">mode_edit</i>
+      </a>
+    </div>
+  );
 }
 
-const mapStateToProps = ({ members: { self, }, }) => ({
-  self,
-});
-
-const mapDispatchToProps = dispatch => ({
-  // uploadPhoto: membersOperations.uploadhoto,
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Avatar);
+export default Avatar;
