97d9a690d7c978042e55bc077ce5b5a002afe62c
Thomas Perale <thomas.perale@openmailbox.org>
2017-10-14 18:11:57 +0200

    [enh] Add back the default avatar if none

--- assets/js/profile/components/Avatar.js
+++ assets/js/profile/components/Avatar.js
@@ -3,11 +3,13 @@ import { connect } from 'react-redux';
 // import { membersOperations } from '../ducks/members/';
 import { CardTitle } from 'react-materialize';
 
+const avatarUrl = 'http://www.gravatar.com/avatar/f26f9290731a749a7fbc9ad6fd080827?s=300&d=http%3A%2F%2Fcerkinfo.be%2Fmedia%2F%2Fimages%2Fmembers%2Fdefault-person.png';
+
 const Avatar = ({ avatar, }) => {
   console.log(avatar);
   return (
     <div>
-      <CardTitle reveal image={avatar} waves='light'/>
+      <CardTitle reveal image={avatar ? avatar : avatarUrl } waves='light'/>
       <a href="" className="btn-floating btn halfway-fab waves-effect waves-light red">
         <i className="material-icons">mode_edit</i>
       </a>
